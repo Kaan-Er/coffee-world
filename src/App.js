edb5d55dd01af03d0af7d7e2d8478ebb7064fe23
Kaan Er <erkaan1@outlook.com>
2021-09-17 23:30:16 +0300

    Add sidenav and input component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { Fragment } from "react";
 import "./App.css";
+import Input from "./components/Input/Input";
 
 function App() {
-  return <div>Starting the code :)</div>;
+  return (
+    <Fragment>
+      <div className="sidenav">
+        <Input placeholder="Search" />
+      </div>
+      <div className="main"></div>
+    </Fragment>
+  );
 }
 
 export default App;
---
+++ src/components/Input/Input.js
@@ -0,0 +1,5 @@
+const Input = ({ placeholder }) => {
+  return <input className="inputStyle" placeholder={placeholder} />;
+};
+
+export default Input;
